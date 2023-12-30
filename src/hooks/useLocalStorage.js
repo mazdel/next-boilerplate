6e35e7c80635fadb50a65b279a79e04866909e65
MazDel <65859631+mazdel@users.noreply.github.com>
2023-12-30 08:17:12 +0700

    feat: add custom hook to handle localStorage (#8)

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,30 @@
+"use client";
+import { useState, useEffect } from "react";
+// import { useSyncExternalStore } from "react";
+
+/**
+ * Custom react hook to manage localStorage
+ * @param {string} key localStorage key
+ * @param {string} defaultValue default value of the key
+ * @returns {[any,function]}
+ */
+export const useLocalStorage = (key, defaultValue) => {
+  const [value, setValue] = useState(defaultValue);
+
+  const changeValue = (value) => {
+    setValue(value);
+    localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  useEffect(() => {
+    const stored = localStorage.getItem(key);
+    if (!stored) {
+      setValue(defaultValue);
+      localStorage.setItem(key, JSON.stringify(defaultValue));
+    } else {
+      setValue(JSON.parse(stored));
+    }
+  }, [defaultValue, key]);
+
+  return [value, changeValue];
+};
